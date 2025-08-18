578e9455013c151377c4e46e45f7f8038da203b8
Jazmin Gomez <gomjazmin12@gmail.com>
2025-08-18 19:10:46 +0000

    ...

--- Checkpoint 2/hello2.py
+++ Checkpoint 2/hello2.py
@@ -1,6 +1,8 @@
-def hello():
-    print("hello")
+def main():
+    name = input("What is your name?").strip().title()
+    hello(name)
 
-name= input("What is your name? ").strip().title()
-hello()
-print(f"hello, {name}")
+def hello(to="world"):
+    print(f"hello, {to}")
+
+
