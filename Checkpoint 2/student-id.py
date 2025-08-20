537029cbea1d591a300ec53d7b9d40fcd5a00a0e
Jazmin Gomez <gomjazmin12@gmail.com>
2025-08-20 20:20:03 +0000

    ---

---
+++ Checkpoint 2/student-id.py
@@ -0,0 +1,19 @@
+def main():
+	print('')
+	print('Please enter the following information to create your ID Card.')
+	input('Press enter to continue.')
+	
+    name = input("Your Name")
+    Lastname = input("Your Last name")
+    EmailAddress = input("Your Email Address")
+    PhoneNumber = input("Your Phone number")
+    StudentIDNumber = input("Student ID Number") 
+    FPTClass = input("Your FPT CLass")
+    ExpectedGraduationYear = input("Your Graduation Year")
+    FavoriteSubject=input ("Your Favorite Subject")
+    ExtracurricularActivities= input ("You have extracurricular activities? ")
+	
+	print("Your ID Card is:"\n)
+	line = "-" * 45
+	print(line)
+	print(Lastname.)
