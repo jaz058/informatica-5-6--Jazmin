e0fcf56d57e0e30a6f3e924dde0c22fb730a9f2f
Jazmin Gomez <gomjazmin12@gmail.com>
2025-08-21 20:20:07 +0000

    ...

--- Checkpoint 2/if-exercises.py
+++ Checkpoint 2/if-exercises.py
@@ -1,4 +1,15 @@
 def main():
+    number= int(input("Enter a integer number: "))
+    integer_number(number)
+
+def integer_number():
+  if number > 0  
+
+
+
+
+
+def main():
     x= int(input("Type a number: "))
     print("Your number squared is", square (x))
 
