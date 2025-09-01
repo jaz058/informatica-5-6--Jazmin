50a90608248b56df97073313e38a279859c3f8c3
Jazmin Gomez <gomjazmin12@gmail.com>
2025-09-01 19:37:32 +0000

    ....

--- Checkpoint 3/..py
+++
@@ -1,7 +0,0 @@
-import time
-
-print("Starting program...")
-time.sleep(1)  # Pause execution for 1 second
-print("Program continues after 1 second.")
-time.sleep(0.5) # Pause execution for 0.5 seconds
-print("Program continues after another 0.5 seconds.")
---
+++ Checkpoint 4/adv-strings.py
@@ -0,0 +1,28 @@
+# #String Length
+input_string=input("Please type a message: ")
+# print(input_string)
+
+# print("-" * len(input_string))
+
+
+# #String Index
+# print(input_string[0])
+# print(input_string[1])
+# print(input_string[3])
+# print(input_string[-1])
+
+# #Example
+# i=0
+# while i < len(input_string):
+#     print(input_string[i])
+#     i += 1
+
+# #Example 
+# i = 0
+# while i < len(input_string):
+#     print(input_string):
+#     i=1
+
+print("h" in input_string)
+print("j" in input_string)
+print("hello" in input_string)
