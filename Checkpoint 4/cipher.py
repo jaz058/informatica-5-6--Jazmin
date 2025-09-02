222a2ecd2c6edb36aff4d96d43aff4c51392855a
Jazmin Gomez <gomjazmin12@gmail.com>
2025-09-02 16:40:02 +0000

    mmmm

---
+++ Checkpoint 4/cipher.py
@@ -0,0 +1,13 @@
+def main():
+    message=("Write a message that would be encode: ").lower()
+    encode_message=(message)
+            
+def encode_message(text):
+     alphabet=("abcdefghijklmnopqrstuvwxyz")
+     cipher=("zyxwvutsrqponmlkjihgfedcba")
+     new_message=""
+     i=0
+
+     while i < len(text)
+     char=text[i]
+     
