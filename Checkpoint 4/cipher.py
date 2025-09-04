42ac711269ebbd6191b9bf99906d6f69ba66376d
Jazmin Gomez <gomjazmin12@gmail.com>
2025-09-04 16:36:08 +0000

    ññññññ

--- Checkpoint 4/cipher.py
+++ Checkpoint 4/cipher.py
@@ -1,13 +1,22 @@
 def main():
-    message=("Write a message that would be encode: ").lower()
-    encode_message=(message)
+    message= input("Write a message that would be encode: ").lower()
+    encode_message(message)
             
 def encode_message(text):
-     alphabet=("abcdefghijklmnopqrstuvwxyz")
-     cipher=("zyxwvutsrqponmlkjihgfedcba")
-     new_message=""
-     i=0
+    alphabet=("abcdefghijklmnopqrstuvwxyz")
+    cipher=("zyxwvutsrqponmlkjihgfedcba")
+    new_message=""
+    i=0
 
-     while i < len(text)
-     char=text[i]
+    while i < len(text):
+        char=text[i]
      
+        if char in alphabet:
+            cipher_index = alphabet.find(char)
+            new_message += cipher[cipher_index]
+        else:
+            new_message += char
+        i +=1
+    print('Encoded message:' + new_message)
+
+main()
