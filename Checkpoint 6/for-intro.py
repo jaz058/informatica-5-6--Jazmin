db24dc6d19a24ec1e5cabbb5d642ea44cbbb5f14
Jazmin Gomez <gomjazmin12@gmail.com>
2025-09-17 20:20:27 +0000

    ...

---
+++ Checkpoint 6/for-intro.py
@@ -0,0 +1,12 @@
+doctrine = ["Faith", "Repentance","Baptism","Holy Ghost", "Endure to the end"]
+
+# i=0
+# while i<len(doctrine):
+#     print(doctrine[i])
+#     i +=1
+
+for principle in doctrine:
+    print(principle)
+
+    school_objective= ["Deepen conversion to Jesus Christ and His restored Gospel","Qualify for the blessing of the temple","Prepare for missionery sevice", "Build the kingdom of God" ]
+    
