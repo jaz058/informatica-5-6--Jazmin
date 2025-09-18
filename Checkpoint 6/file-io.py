0685955efb30be0eb68ebbfa5350c06581322087
Jazmin Gomez <gomjazmin12@gmail.com>
2025-09-18 20:15:47 +0000

    oooo

--- Checkpoint 6/file-io.py
+++ Checkpoint 6/file-io.py
@@ -9,7 +9,16 @@
 # for name in sorted(names):
 #     print(f" Hello {name}")
 
-name=input("What´s your name?")
-file= open("name.txt","a")
-file.write(f"{name}\n")
-file.close()
+# name=input("What´s your name?")
+# file= open("name.txt","a")
+# file.write(f"{name}\n")
+# file.close()
+
+# with open("names.txt","a") as file:
+#     file.write(f"{input("Wat´s your name?")}")  
+
+with open("names.txt", "r") as file:
+    lines= file.readlines()
+
+for line in sorted (lines):
+        print(f"Hello{line.rstrip()}")
