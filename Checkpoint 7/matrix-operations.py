dd022e596d52b9b319a1241ad017ce7a587a79eb
Jazmin Gomez <gomjazmin12@gmail.com>
2025-09-23 16:39:25 +0000

    ......

---
+++ Checkpoint 7/matrix-operations.py
@@ -0,0 +1,16 @@
+def sum_of_row(matrix, row_number):
+    row= matrix[row_number]
+    row_sum = 0
+    for item in row:
+        row_sum += item 
+    return row_sum 
+
+def sum_of_column (matrix, column_number: int):
+    column_sum = 0
+    for row in matrix:
+        column_sum = column_sum + row[column_number]
+    return column_sum
+
+m = [[4,2,3,2],[9,1,12,11],[7,8,9,5],[2,9,15,1]]
+muy_sum = sum_of_row(m,1)
+print(muy_sum)
