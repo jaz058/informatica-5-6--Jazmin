0eb8b9eee8b467cd33cabf2ec24aa2ca7422427f
Jazmin Gomez <gomjazmin12@gmail.com>
2025-09-26 17:30:27 +0000

    ñoñoñoño

---
+++ Checkpoint 7/birthdays.py
@@ -0,0 +1,13 @@
+birthdays = {
+    "Luna":"22/01/2008",
+    "Luisa":"25/10/2007",
+    "Jose":"19/05/2008"
+}
+print(birthdays)
+name= input("Select a name:")
+
+if name == "Luna" or "Luisa" or "Jose":
+    print(birthdays[name]) 
+else:
+    print(print(birthdays[name]))
+    
