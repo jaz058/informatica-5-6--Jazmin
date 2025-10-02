27fd4bd9daecbea9ba8f2b7465aece92acd613f3
Jazmin Gomez <gomjazmin12@gmail.com>
2025-10-02 15:45:35 +0000

    ++++

--- Checkpoint 8/character-count.py
+++ Checkpoint 8/character-count.py
@@ -4,6 +4,20 @@ def character_count (message , dictionary):
         dictionary[charecter] += 1
     print(dictionary)
 
-message = input ("Write a message")
-dictionary = {}
-character_count(message, dictionary)
+    print(sum(dictionary.values()))
+
+
+# #Alternative 1
+# dictionary = {}
+# values_list = list(dictionary.values())
+# print(values_list)
+# largest_number_index = values_list.index(max(values_list))
+# repeted_character = list(dictionary.keys())[largest_number_index]
+# print(f"The most repeted character is: {repeted_character}, repeating{dictionary}[repeted_character],times")
+
+# #Alternative 2
+# largest_number2 = max(dictionary,key=dictionary.get)
+# print(f"The most repeted character is: {largest_number2}, repeating{dictionary}[largest_number2],times")
+# message = input ("Write a message")
+
+# character_count(message, dictionary)
