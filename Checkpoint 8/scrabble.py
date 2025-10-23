d9a02e0b37850a15ec852391fdf83ca9cb8168ce
Jazmin Gomez <gomjazmin12@gmail.com>
2025-10-23 16:31:42 +0000

    ssssss

--- Checkpoint 8/scrabble.py
+++ Checkpoint 8/scrabble.py
@@ -1,18 +1,44 @@
 import random
-def main():
-    assign_letters()
-alphabet = {
-    "a":1,"b":3,"c":3,"d":2,"e":1,"f":4,"g":2,"h":4,"i":1,"j":8,"k":5,"l":1,"m":3,"n":1,"o":1,"p":3,"q":10,"r":1,"s":1,"t":1,"u":1,"v":4,"w":4,"x":8,"y":4,"z":10
+
+letter_values = {
+"A": 1, "E": 1, "I": 1, "O": 1, "U": 1, "L": 1, "N": 1, "R": 1, "S": 1, "T": 1,
+"D": 2, "G": 2,
+"B": 3, "C": 3, "M": 3, "P": 3,
+"F": 4, "H": 4, "V": 4, "W": 4, "Y": 4,
+"K": 5,
+"J": 8, "X": 8,
+"Q": 10, "Z": 10,
 }
 
-def assign_letters():
-    print("Welcome to Scrabble!")
-    letters = []
-    for letter in range(13):
-        letters.append((random.choice(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])))
-    print(f"Your letters are", end=" ")
-    for item in letters:
-        print(item, end=", ")
+turn_letters = []
+points = 0
 
+for random_letter in range(13):
+    random_index = random.randint(0, len(letter_values) - 1)
+    letter_list = list(letter_values.keys())
+    new_letter = letter_list[random_index]
+    turn_letters.append(new_letter)
 
-main()
+print("Welcome to Scrabble!")
+print(f"Your letters are:")
+print(turn_letters)
+
+while True:
+    print("Type your word (blank to finish the game):")
+    word_attempt = input().upper()
+    if word_attempt == "":
+        break
+    if set(word_attempt).issubset(set(turn_letters)):
+        for l in range(len(word_attempt)):
+            points += letter_values[word_attempt[l]]
+            turn_letters.remove(word_attempt[l])
+            turn_letters.append(random.choice(list(letter_values.keys())))
+    else:
+        print("Please type a word that uses your letters.") 
+
+    print("Points:", points)
+    print("Remaining letters:")
+    print(turn_letters)
+        
+print("GAME OVER")
+print("Total points:", points)
