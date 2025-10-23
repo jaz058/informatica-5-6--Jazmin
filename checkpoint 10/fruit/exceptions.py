07afb5a4d7ef7ee8330ec9ece11bd8ca364be91b
Jazmin Gomez <gomjazmin12@gmail.com>
2025-10-23 19:57:06 +0000

    111

--- checkpoint 10/fruit/exceptions.py
+++ checkpoint 10/fruit/exceptions.py
@@ -7,10 +7,22 @@
 # except ValueError:
 #     print("x is not a number")
 
-def spam(divided_by):
-    return 42 / divided_by
+# def spam(divided_by):
+#     return 42 / divided_by
 
-print(spam(2))
-print(spam(12))
-print(spam(0))
-print(spam(1))
+# print(spam(2))
+# print(spam(12))
+# print(spam(0))
+# print(spam(1))
+
+def read_small_integer():
+    while True:
+        try:
+            input_str = input("Please type in an integer: ")
+            number = int(input_str)
+            if number < 100 and number >= 0:
+                return number 
+        except ValueError :
+            pass
+number = read_small_integer()
+print(number, "to the power of three is", number**3)
