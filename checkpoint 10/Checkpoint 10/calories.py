3b9378512ca1f597ebf6bf32c4a17862c6aef2a5
Jazmin Gomez <gomjazmin12@gmail.com>
2025-10-30 20:19:51 +0000

    mmmm

---
+++ checkpoint 10/Checkpoint 10/calories.py
@@ -0,0 +1,13 @@
+def main():
+    calories_list = [
+        "Half and Half, fat free" : "9",
+        "Almond milk " : "30",
+        "" : "" 
+        "Plain, low-fat" : "77" 
+        "Sour Cream, fat free" : "9"
+        "egg " : "75"
+        "egg white" : "16"
+        "Cottage Cheese, low fat" : "20"
+        "Cream Cheese" : "51"
+           
+    ]
