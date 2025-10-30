c21a7187e6067aee040a36b9f4a859a46d0cb835
Jazmin Gomez <gomjazmin12@gmail.com>
2025-10-30 17:28:43 +0000

    yyyy

---
+++ checkpoint 10/Checkpoint 10/factorial.py
@@ -0,0 +1,10 @@
+# def divide(a,b):
+#     if b==0:
+#         raise ValueError("Hey! You cannot divide by zero.")
+#     return a / b
+
+# print(divide(1,2))
+# print(divide(2,0))
+
+def main():
+    
