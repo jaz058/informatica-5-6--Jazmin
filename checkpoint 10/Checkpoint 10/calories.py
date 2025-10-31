4f330887260b3210d03fb70c9a3d4673ad6c953b
Jazmin Gomez <gomjazmin12@gmail.com>
2025-10-31 17:28:32 +0000

    iii

--- checkpoint 10/Checkpoint 10/calories.py
+++ checkpoint 10/Checkpoint 10/calories.py
@@ -1,13 +1,14 @@
 def main():
-    calories_list = [
-        "Half and Half, fat free" : "9",
-        "Almond milk " : "30",
-        "" : "" 
-        "Plain, low-fat" : "77" 
-        "Sour Cream, fat free" : "9"
-        "egg " : "75"
-        "egg white" : "16"
-        "Cottage Cheese, low fat" : "20"
-        "Cream Cheese" : "51"
-           
-    ]
+    calories_list = {
+        "Half and Half, fat free" : 9,
+        "Almond milk " : 30,
+        "Watermellon" : 11, 
+        "Plain, low-fat" : 77, 
+        "Sour Cream, fat free" : 9,
+        "egg " : 75,
+        "egg white" : 16,
+        "Cottage Cheese, low fat" : 20,
+        "Cream Cheese" : 51,
+        "Whole" : 73,  
+    }
+    print(calories_list)
--- checkpoint 10/Checkpoint 10/factorial.py
+++ checkpoint 10/Checkpoint 10/factorial.py
@@ -5,6 +5,16 @@
 
 # print(divide(1,2))
 # print(divide(2,0))
-
 def main():
-    
+    factorial()
+
+def factorial(n):
+    if n <= 0:
+        raise ValueError(f"The input was negative or zero: {n}")
+    else:
+        new_n = 1
+        print(list(range(2, n + 1)))
+        for 1 in range(2, n + 1):
+            new_n = new_n*1
+        return new_n 
+main()   
