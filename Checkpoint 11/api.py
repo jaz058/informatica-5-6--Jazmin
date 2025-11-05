bae4566b43dfd2a4d29c938998133853808e0159
Jazmin Gomez <gomjazmin12@gmail.com>
2025-11-05 20:14:02 +0000

    sas

---
+++ Checkpoint 11/api.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+song= input("What song are you looking for? ")
+
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=20&term="+song)
+# print(response.json())
+# print(json.dumps(response.json(), indent=2))
+
+search = response.json()
+for result in search["results"]:
+    print(result["trackName"],result["artistName"])
+
+city = input("City: ")
+response2 = requests.get("http://goweather.xyz/weather/"+city)
+print(response2.json())
