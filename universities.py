41021b2fbb1b1b52a614ec4f841a0b415216ddca
Jazmin Gomez <gomjazmin12@gmail.com>
2025-11-11 16:39:45 +0000

    nn

---
+++ universities.py
@@ -0,0 +1,18 @@
+def main():
+    universities_list = {
+        "URN":{
+        "careers":"3",
+        "cost":"3,600.00 MXN al mes aproximadamente",
+        "nearest one": "25 km"
+        }
+        "UPNECH":{
+        "carrers": "4",
+        "cost":"3,300 MXN"
+        "nearest one": "26 km"
+        }
+        "EAC":{
+        
+
+        }
+
+    }
