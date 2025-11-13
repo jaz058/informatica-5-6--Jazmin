dbeb8d01a9079120d65c5e3cbb1f77031dfe31ae
Jazmin Gomez <gomjazmin12@gmail.com>
2025-11-13 20:20:39 +0000

    nnn

--- Checkpoint 12/problem-api.py
+++ Checkpoint 12/problem-api.py
@@ -1,7 +1,10 @@
 import requests
 import json
-print(("🩷 "*25))
+print(("🩷 🧡 ")*9)
 
 product = (input("What is the product you are searching for??"))
+free_list=
+free=(input("What type of free are you looking for?"))
 response =  requests.get("http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline")
-look =
+look = (response.json()["name"]["product_type"]["description"])
+print("Your pruduct is :"{look})
